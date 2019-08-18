3d9f480553f463852cb02c176729a9b2697dabc4
derfaq <derfaq.serg@gmail.com>
2019-08-18 13:27:27 +0300

    добавил проверку resultCode

--- app/src/main/java/com/example/home_work_c2_l3/MainActivity.java
+++ app/src/main/java/com/example/home_work_c2_l3/MainActivity.java
@@ -3,15 +3,14 @@ package com.example.home_work_c2_l3;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int COLOR_REQUEST_CODE = 1;
     FrameLayout frame;
     Button button;
 
@@ -27,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, RgbSelector.class);
-                startActivityForResult(intent, 1);
+                startActivityForResult(intent, COLOR_REQUEST_CODE);
             }
         });
 
@@ -35,12 +34,22 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        int colorR = data.getIntExtra("colorR",0);
-        int colorG = data.getIntExtra("colorG",0);
-        int colorB = data.getIntExtra("colorB",0);
-        frame.setBackgroundColor(0xff000000 + colorR * 0x10000 + colorG * 0x100
-                + colorB);
-        button.setText("Сменить еще раз");
+
+        switch (requestCode){
+            case COLOR_REQUEST_CODE:
+                if(resultCode != RESULT_OK){
+                    return;
+                } else{
+                    int colorR = data.getIntExtra("colorR",0);
+                    int colorG = data.getIntExtra("colorG",0);
+                    int colorB = data.getIntExtra("colorB",0);
+                    frame.setBackgroundColor(0xff000000 + colorR * 0x10000 + colorG * 0x100
+                            + colorB);
+                    button.setText("Сменить еще раз");
+                }
+        }
+
+
     }
 
 }
